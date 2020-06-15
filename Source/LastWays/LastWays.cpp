c03f1ad61bc5d6f26131f5825765f9967e7b0549
Aymen <aymen.kmama@gmail.com>
2020-06-15 03:09:57 +0200

    LastWays
    Android Game

---
+++ Source/LastWays/LastWays.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "LastWays.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, LastWays, "LastWays" );
---
+++ Source/LastWays/LastWaysGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "LastWaysGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LASTWAYS_API ALastWaysGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
